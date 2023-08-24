02a5bc01bd202294bd40e89895db9d4bacd8f929
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-08-24 13:11:15 -0600

    adding movie crud files

---
+++ laboratorios/laboratorio4/laboratorio4/laboratorio4/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using laboratorio4.Data;
+using laboratorio4.Models;
+
+namespace laboratorio4.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly laboratorio4.Data.laboratorio4Context _context;
+
+        public CreateModel(laboratorio4.Data.laboratorio4Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Movie == null || Movie == null)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ laboratorios/laboratorio4/laboratorio4/laboratorio4/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using laboratorio4.Data;
+using laboratorio4.Models;
+
+namespace laboratorio4.Pages.Movies {
+    public class IndexModel : PageModel {
+        private readonly laboratorio4.Data.laboratorio4Context _context;
+
+        public IndexModel(laboratorio4.Data.laboratorio4Context context) {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync() {
+            if (_context.Movie != null) {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
