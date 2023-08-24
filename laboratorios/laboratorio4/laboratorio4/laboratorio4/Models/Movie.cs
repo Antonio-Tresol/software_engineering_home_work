5b2daa797044fac117c1687697933ec704e12d7c
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-08-24 13:10:47 -0600

    adding data model movie

---
+++ laboratorios/laboratorio4/laboratorio4/laboratorio4/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace laboratorio4.Models {
+    public class Movie {
+        public int Id { get; set; }
+        // the question mark after the data type means attribute is nullable
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
