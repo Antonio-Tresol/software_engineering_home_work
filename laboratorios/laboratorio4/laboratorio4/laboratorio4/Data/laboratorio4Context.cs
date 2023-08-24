8c73f5dfa2d148f8fe9ed78c2515a190132995f0
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-08-24 13:12:25 -0600

    adding data base file after setting data model to include movie

---
+++ laboratorios/laboratorio4/laboratorio4/laboratorio4/Data/laboratorio4Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using laboratorio4.Models;
+
+namespace laboratorio4.Data {
+    public class laboratorio4Context : DbContext {
+        public laboratorio4Context (DbContextOptions<laboratorio4Context> options)
+            : base(options) { }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+    }
+}
