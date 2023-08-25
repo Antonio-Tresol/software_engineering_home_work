60649e1eba2710f95f386e7bb0f8955a539c4f3e
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-08-25 02:12:31 -0600

    adding search to song index

--- laboratorios/laboratorio4/laboratorio4/laboratorio4/Pages/Songs/Index.cshtml.cs
+++ laboratorios/laboratorio4/laboratorio4/laboratorio4/Pages/Songs/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using laboratorio4.Data;
 using laboratorio4.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace laboratorio4.Pages.Songs {
     public class IndexModel : PageModel {
@@ -18,10 +19,32 @@ namespace laboratorio4.Pages.Songs {
 
         public IList<Song> Song { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Singers { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? songSinger { get; set; }
+
         public async Task OnGetAsync() {
-            if (_context.Song != null) {
-                Song = await _context.Song.ToListAsync();
+            // Use LINQ to get list of genres.
+            IQueryable<string> singerQuery = from m in _context.Song
+                                            orderby m.Singer
+                                            select m.Singer;
+
+            var songs = from m in _context.Song
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString)) {
+                songs = songs.Where(s => s.Name.Contains(SearchString));
             }
+
+            if (!string.IsNullOrEmpty(songSinger)) {
+                songs = songs.Where(x => x.Singer == songSinger);
+            }
+            Singers = new SelectList(await singerQuery.Distinct().ToListAsync());
+            Song = await songs.ToListAsync();
         }
     }
 }
