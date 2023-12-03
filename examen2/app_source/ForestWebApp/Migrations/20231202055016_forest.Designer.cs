66b388643574098add86852c4aff50a79c5ce26a
Antonio Badilla-Olivas <84429050+Antonio-Tresol@users.noreply.github.com>
2023-12-02 22:07:48 -0600

    disable a warning of migrations that are autogenerated

--- examen2/app_source/ForestWebApp/Migrations/20231202055016_forest.Designer.cs
+++ examen2/app_source/ForestWebApp/Migrations/20231202055016_forest.Designer.cs
@@ -13,7 +13,9 @@ namespace ForestWebApp.Migrations
 {
     [DbContext(typeof(ForestWebAppContext))]
     [Migration("20231202055016_forest")]
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     partial class forest
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
