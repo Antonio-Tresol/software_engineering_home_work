c62d91a22cac093939de7627df26b60335f1cbf6
Antonio Badilla-Olivas <84429050+Antonio-Tresol@users.noreply.github.com>
2023-12-02 22:14:27 -0600

    Adds data anotations to forest entity

--- examen2/app_source/ForestWebApp/Models/Forest.cs
+++ examen2/app_source/ForestWebApp/Models/Forest.cs
@@ -4,46 +4,53 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace ForestWebApp.Models;
 
 /// <summary>
-///   a Forest information
+///     a Forest information
 /// </summary>
 public class Forest
 {
     /// <summary>
-    ///    An unique identifier
+    ///     An unique identifier
     /// </summary>
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; }
-    
+
     /// <summary>
     ///     The forest's name
     /// </summary>
-    [Required]
-    [StringLength(60, MinimumLength = 1)]
-    public string Name { get; set; }
+    [Required(ErrorMessage = "El nombre del bosque es obligatorio.")]
+    [StringLength(60, MinimumLength = 1, ErrorMessage = "La longitud del nombre debe estar entre 1 y 60 caracteres.")]
+    [Display(Name = "Nombre")]
+    public string Name { get; set; } = "";
 
     /// <summary>
     ///     The country where the forest is located
     /// </summary>
-    [Required]
-    [StringLength(60, MinimumLength = 1)]
-    public string CountryOfOrigin { get; set; }
+    [Required(ErrorMessage = "El país de origen es obligatorio.")]
+    [StringLength(60, MinimumLength = 1,
+        ErrorMessage = "La longitud del país de origen debe estar entre 1 y 60 caracteres.")]
+    [Display(Name = "País de Origen")]
+    public string CountryOfOrigin { get; set; } = "";
 
     /// <summary>
     ///     The Forest's vegetation type
     /// </summary>
-    [Required]
-    [StringLength(60, MinimumLength = 1)]
-    public string TypeOfVegetation { get; set; }
+    [Required(ErrorMessage = "El tipo de vegetación es obligatorio.")]
+    [StringLength(60, MinimumLength = 1,
+        ErrorMessage = "La longitud del tipo de vegetación debe estar entre 1 y 60 caracteres.")]
+    [Display(Name = "Tipo de Vegetación")]
+    public string TypeOfVegetation { get; set; } = "";
 
     /// <summary>
     ///     The forest's area in square kilometers
     /// </summary>
-    [Required]
+    [Required(ErrorMessage = "El área en kilómetros cuadrados es obligatoria.")]
+    [Display(Name = "Área (Km²)")]
     public int AreaKm2 { get; set; } = 0;
 
     /// <summary>
     ///     Indicates
     /// </summary>
-    [Required]
+    [Required(ErrorMessage = "Es necesario indicar si es un bosque primario.")]
+    [Display(Name = "Bosque Primario")]
     public bool OldGrowthForest { get; set; } = true;
 }
